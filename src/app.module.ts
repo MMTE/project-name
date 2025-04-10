6247780d5aab8cd60ed186594ecc4c618afabddc
MMTE <hi.taleghani@gmail.com>
2025-04-10 11:16:12 +0330

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DbCheckService } from './db-check.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, DbCheckService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('POSTGRES_HOST'),
+        port: parseInt(config.get('POSTGRES_PORT') || '5432'),
+        username: config.get('POSTGRES_USER'),
+        password: config.get('POSTGRES_PASSWORD'),
+        database: config.get('POSTGRES_DB'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+  providers: [DbCheckService], // where you inject DataSource
 })
 export class AppModule {}
